9ed76d831b5e9e586606dd2359618b690b831248
Rahul Sharma <rahul.sharma@monkhub.com>
2022-07-22 10:32:24 +0530

    fronend update

--- src/components/ConnectWallet.js
+++ src/components/ConnectWallet.js
@@ -38,17 +38,17 @@ const ConnectWallet = () => {
       axios.post('https://brew-api.immodesta.com/get-jwt',{publicKey}).then((res)=>{
         console.log(res)
         localStorage.setItem("jwt_token",res.data.jwtToken)
-        try {
+        if(res.data.jwtToken){
           axios.get('https://brew-api.immodesta.com/video-url',{
           headers: { Authorization: `Bearer ${res.data.jwtToken}` }})
           .then((res2)=>{
-            console.log(res2)
-            localStorage.setItem("User_Auth",true)
-            navigate("/accessgranted")
+              console.log(res2)
+              localStorage.setItem("User_Auth",true)
+              navigate("/accessgranted")
+          }).catch((err)=>{
+            console.log(err)
+            navigate('/accessdenied')    
           })
-        } catch (error) {
-          // navigate('/accessdenied')
-          console.log(error)
         }
       })
     }
@@ -66,8 +66,8 @@ const ConnectWallet = () => {
         <div className='my-5'>
           <span style={{ color: "#4AABE9", fontWeight: "700", fontSize: "42px" }}>#DiveIn</span>
         </div>
-        <div className="d-flex flex-row align-items-center">
-          <img src={connectwalletBtn} className="my-2" style={{ cursor: "pointer" }} height={65} onClick={() => authenticate({ signingMessage: "Hello youtube" })} alt="" srcSet="" />
+        <div className="d-flex flex-row flex-wrap align-items-center">
+          <img src={connectwalletBtn} className="my-2" style={{ cursor: "pointer" }} height={55} onClick={() => authenticate({ signingMessage: "Hello youtube" })} alt="" srcSet="" />
           <button className='wallet-connect-btn ms-2' onClick={() => authWalletConnect()}>Sign using Wallet connect</button>
         </div>
         <span style={{ fontWeight: "700", color: "white" }}>with a valid event pass NFT.</span>
